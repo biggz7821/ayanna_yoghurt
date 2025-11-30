46717ff79be240fe97d92de3c2e7257281d7e555
biggz7821 <allansquake@gmail.com>
2025-11-30 13:09:56 +0300

    Update urls.py

--- ayannayoghurt/urls.py
+++ ayannayoghurt/urls.py
@@ -1,1 +1,8 @@
-from django.contrib import admin from django.urls import path, include urlpatterns = [ path('admin/', admin.site.urls), # Django built-in admin path('', include('orders.urls')), # Your custom URLs (customer + manage) ]
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('__fix_admin__/', fix_admin),  # TEMPORARY FIX URL
+    path('', include('orders.urls')),
+]
